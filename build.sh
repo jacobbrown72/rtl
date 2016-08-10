8eafb68d5be3144f01923b564463d1c4a19789ef
Jacob Brown <jacobhbrown0826@gmail.com>
2016-08-10 18:46:01 -0400

    Removed unecessary file and added build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -d "work" ]; then
+    echo "Creating work ModelSim work directory"
+    vlib work
+else
+    echo "ModelSim work directory already exists"
+fi
+
+echo "Compiling vhdl files"
+vcom alu.vhd
+vcom databus.vhd
+vcom pc.vhd
+vcom pswreg.vhd
+vcom spreg.vhd
+vcom regsel.vhd
+vcom iobus.vhd
+vcom databus.vhd
+vcom rom.vhd
+vcom ram.vhd
+vcom datapath.vhd
+vcom controller.vhd
+vcom avr8.vhd
+vcom avr_tb.vhd
